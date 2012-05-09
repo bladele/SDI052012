699cfc8ff3a307db1a6c16ff52d354b7863dee83
Bodunrin Ladele <bladele@me.com>
2012-05-09 12:22:07 -0400

    The procedure is working as will as my boolean function. Yay me!

--- ladele_bodunrin_assignment2.js
+++ ladele_bodunrin_assignment2.js
@@ -2,27 +2,41 @@
 // Author: Bodunrin Ladele
 // Created for: SDI Online
 
-
 var name = "Bodunrin Ladele",
+	birthDate = 1979,
 	birthPlace = "Ibandan, Nigeria",
 	chldHdTrauma = true,
 	disftnlWrld = ["curruption", "violence", "poverty", "greed", "misfortune"],
 	liberator = ["love"],
-	paralzr = ["fear", "self doubt", "self pity"],
+	paralzr = ["fear", "self doubt", "self pity"]
+;
 
 
+//Begin the story.
+console.log("My name is " + name + " and I was born in " + birthPlace + " in " + birthDate + ".");
 
-
+//Prodedure.
 var doIHaveHope = function(chldHdTrauma, disftnlWrld) {
-	var chldHdTrauma - true;
+	var chldHdTrauma = true;
 	var disftnlWrld = ["curruption", "violence", "poverty", "greed", "misfortune"];
-	if ((chldHdTrauma = true) && (disftnlWrld.length > liberator.length))
-}
+	if ((chldHdTrauma = true) && (disftnlWrld.length > liberator.length)) {
+		return lifeWithPurps;
+	}	else {
+		return whatToDo;
+	}
+};
 
 
 
+//Boolean function
+var whatToDo = function(humbLove, originOutput) {
+	var originOutput = ["family", "friends", "significant others", "work", "play", "socialization"];
+	var humbLove = originOutput.length * 2;
+	if (humbLove > disftnlWrld.length) {
+		console.log("I found the answer.");
+	} else {
+		console.log("Life is hopeless.")
+	}
+};
 
-
-
-
-
+whatToDo();
