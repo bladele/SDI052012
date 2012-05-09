460e7437257f4c3a92740f2629e87609c7beb860
Bodunrin Ladele <bladele@me.com>
2012-05-09 13:33:55 -0400

    Number function is done, I need to revisit my procedure output.

--- ladele_bodunrin_assignment2.js
+++ ladele_bodunrin_assignment2.js
@@ -26,8 +26,6 @@ var doIHaveHope = function(chldHdTrauma, disftnlWrld) {
 	}
 };
 
-
-
 //Boolean function
 var whatToDo = function(humbLove, originOutput) {
 	var originOutput = ["family", "friends", "significant others", "work", "play", "socialization"];
@@ -38,5 +36,22 @@ var whatToDo = function(humbLove, originOutput) {
 		console.log("Life is hopeless.")
 	}
 };
-
 whatToDo();
+
+//Number function
+var timeItook = function(birthDate) {
+	var birthDate = 1979;
+	while (birthDate < 2012) {
+		console.log("In " + birthDate + ", I had no answer to life's big question.");
+		birthDate++;
+	}
+};
+timeItook();
+
+console.log("So 2012 is the year of clarity.");
+
+
+
+
+
+
