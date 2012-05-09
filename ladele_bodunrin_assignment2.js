17e9469f33cc99a456b8deb2bba0f11c1b992b95
Bodunrin Ladele <bladele@me.com>
2012-05-09 11:09:22 -0400

    Posting files for assignment 2.

---
+++ ladele_bodunrin_assignment2.js
@@ -0,0 +1,28 @@
+// Finding Purpose
+// Author: Bodunrin Ladele
+// Created for: SDI Online
+
+
+var name = "Bodunrin Ladele",
+	birthPlace = "Ibandan, Nigeria",
+	chldHdTrauma = true,
+	disftnlWrld = ["curruption", "violence", "poverty", "greed", "misfortune"],
+	liberator = ["love"],
+	paralzr = ["fear", "self doubt", "self pity"],
+
+
+
+
+var doIHaveHope = function(chldHdTrauma, disftnlWrld) {
+	var chldHdTrauma - true;
+	var disftnlWrld = ["curruption", "violence", "poverty", "greed", "misfortune"];
+	if ((chldHdTrauma = true) && (disftnlWrld.length > liberator.length))
+}
+
+
+
+
+
+
+
+
