a9d901448f115d422c022d81f57155a171ef992d
Bodunrin Ladele <bladele@me.com>
2012-05-16 01:07:06 -0400

    All the files I need

---
+++ Week 3/js/Ladele_Bodunrin_assignment3.js
@@ -0,0 +1,7 @@
+// 
+// Author: Bodunrin Ladele
+// Created for: SDI Online
+// Control File
+
+console.log(json.journeyMan.name);
+
---
+++ Week 3/js/Ladele_Bodunrin_assignment3_jsondata.js
@@ -0,0 +1,16 @@
+// 
+// Author: Bodunrin Ladele
+// Created for: SDI Online
+// Data File
+
+var json = {
+ 	"journeyMan": [ 
+ 		{
+ 			"uuid" : "001",
+ 			"name": "Bibi",
+ 			"sex": "male",
+ 			"birthYr" : 1979,
+ 			"activity" : ["news", " gaming", " reading", " reference", " utility", " entertainment", "communication", "productivity"],
+ 		}
+ 	]
+};
