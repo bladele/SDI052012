288c50d3a84d33aad3898eb1fe40e059cd99d419
Bodunrin Ladele <bladele@me.com>
2012-05-16 15:55:09 -0400

    Trying to find the best way to organize my data so I can cover on the requirements.

--- Week 3/js/Ladele_Bodunrin_assignment3.js
+++ Week 3/js/Ladele_Bodunrin_assignment3.js
@@ -5,11 +5,44 @@
 
 
 // Frist let us see if my files are properly linked.
-console.log(json.journeyMan[0].theGood[4]);
-var me = json.journeyMan[0].name;
-var birthPlace = json.journeyMan[0].birthPlace;
-var birthDate =json.journeyMan[0].birthYr;
+console.log(json.journeyMan[0].theGood[4].length);
+
+var me = json.journeyMan[0].name,
+	birthPlace = json.journeyMan[0].birthPlace,
+	birthDate =json.journeyMan[0].birthYr
+;
+
+//Object within control file so I can creat methods.
+var lifeExprnce = {
+	chldHdTrauma: "true",
+	chldHdComfrt: "true",
+	disftnlWrld: ["curruption", "violence", "poverty", "greed", "misfortune"],
+	paralzr: ["fear", "self doubt", "self pity"],
+
+};
 
 // Start: The story begins
 console.log("My name is " + me + " and I was born in " + birthPlace + " in " + birthDate + ".");
+
+// For Loop
+var handleData = function(json) {
+	for (var i = 0; i < json.journeyMan[0].theGood.length; i++) {
+		var journeyMan = json.journeyMan[0].theGood[i];
+		console.log(journeyMan);
+	};
+};
+handleData(json);
+
+// While Loop
+var timeItook = function(birthDate) {
+	timeDiff = 2002 - birthDate;
+	while (birthDate < 2002) {
+		console.log("In " + birthDate + ", I had no answer to life's big question.");
+		birthDate++;
+	}
+	console.log("So 2002 is the year of clarity. It only took " + timeDiff + " years.");
+
+};
+timeItook(1979); 
+
 
