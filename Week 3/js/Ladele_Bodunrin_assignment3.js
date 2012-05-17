f3098601ad2abf3a28d0cd4c94c98ec57ab7f64c
Bodunrin Ladele <bladele@me.com>
2012-05-17 14:23:02 -0400

    Almost done.

--- Week 3/js/Ladele_Bodunrin_assignment3.js
+++ Week 3/js/Ladele_Bodunrin_assignment3.js
@@ -4,9 +4,6 @@
 // Control File
 
 
-// Frist let us see if my files are properly linked.
-console.log(json.journeyMan[0].name);
-
 //Variables that allow me to call json data.
 var me = json.journeyMan[0].name,
 	birthPlace = json.journeyMan[0].birthPlace,
@@ -14,7 +11,11 @@ var me = json.journeyMan[0].name,
 ;
 
 // Start: The story begins
-console.log("My name is " + me + " and I was born in " + birthPlace + " in " + birthDate + ".");
+//Method Accessor.
+for (var key in json.journeyMan[0]) {
+	console.log("My " + key + " is " + json.journeyMan[0][key]);
+};
+
 
 // Object within control file so I can creat methods.
 var myVars = {
@@ -29,7 +30,8 @@ var myVars = {
 
 /*Since I am having trouble with making my method work, I need to trouble shoot, and work with something simpler,
 that will work and build from that. I guest that worked.	*/
-//Method
+
+//Method function with nested conditional. 
 	lifePrspctv: function() {
 		console.log("The experiences I had as a child and what I saw in the world made me wonder, 'What is the point?'");
 		var cons = myVars.chldHdTrauma.length + myVars.disftnlWrld.length + myVars.disftnInWrld.length;
@@ -50,25 +52,51 @@ that will work and build from that. I guest that worked.	*/
 myVars.lifePrspctv();
 
 
-// For Loop
+// For Loop with array argument. The array is a property in my json data object.
 var handleData = function(json) {
-	for (var i = 0; i < json.journeyMan[0].theGood.length; i++) {
-		var journeyMan = json.journeyMan[0].theGood[i];
-		console.log(journeyMan);
+	for (var i = 0; i < json.journeyMan[0].societyGoals.length; i++) {
+		var journeyMan = json.journeyMan[0].societyGoals[i];
+		console.log("I knew that if I work hard in life it could get me " + journeyMan + ".");
 	};
 };
 handleData(json);
 
-// While Loop
+
+console.log("But I knew these things would never make me happy or give me purpose.");
+
+// While Loop with number argument. The number is a property in json data object.
 var timeItook = function(birthDate) {
 	timeDiff = 2002 - birthDate;
 	while (birthDate < 2002) {
 		console.log("In " + birthDate + ", I had no answer to life's big question.");
 		birthDate++;
 	}
-	console.log("So 2002 is the year of clarity. It only took " + timeDiff + " years.");
+	console.log("In 2002 I expereince a catalyst event. This event made me begin to see with a perpective beyond myself. It only took " + timeDiff + " years.");
 
 };
 timeItook(1979); 
+
+console.log("Knowing the elements the create happiness in life, didn't empower me to love myself or give me purpose.");
+
+//Method Mutator
+myVars.lifePrspctv = function(thirdEye) {
+	var revelation = myVars.catalyst.length * thirdEye;
+	if (myVars.paralzr.length > revelation) {
+		console.log("Without purpose, I failed at the endevors I pursued and lost hope.");
+	} else {
+		console.log("On a trip through the Appalchian mountains, I witness a moment of awe and beauty that changed my perpective.");
+		console.log("Afterwards, I was able to view the world outside of myself, outside of what I thought to look for in life.");
+	}
+
+};
+myVars.lifePrspctv(3);
+
+
+
+
+
+
+
+
 
 
--- Week 3/js/Ladele_Bodunrin_assignment3_jsondata.js
+++ Week 3/js/Ladele_Bodunrin_assignment3_jsondata.js
@@ -10,12 +10,7 @@ var json = {
  			"gender": "male",
  			"birthPlace" : "Ibandan, Nigeria",
  			"birthYr" : 1979,
- 			"liveIn" : "Concord, NC",
- 			"chldHdTrauma" : true,
- 			"disftnlWrld" : ["curruption", "violence", "poverty", "greed", "misfortune"],
- 			"goodLife" :["family", "friends", "work", "play", "socialization"],
- 			"theGood" : ["love", "friendship", "fullfilment", "joy", "empathy"],
- 			"catalyst" : "sunset"
+ 			"societyGoals" : ["success", "wealth", "fame", "comfort"]
  			
  		}
  	]
