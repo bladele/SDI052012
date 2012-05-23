e15f387c95a5d15cff4d425c3daeb7b595968ef5
Bodunrin Ladele <bladele@me.com>
2012-05-23 16:05:27 -0400

    Need 2 more functions.

--- Week 4/Ladele_Bodunrin_assignment4.js
+++ Week 4/Ladele_Bodunrin_assignment4.js
@@ -59,23 +59,38 @@ bibiLib.stringToNumb = function(strNum){
 	var theNum = parseInt(strNum);
 	var theResult = theNum; 
 	if(isNaN(strNum)) {
-		return NaN;
-	} else {
-		return theResult;
-	}
+			return NaN;
+			} else {
+			return theResult;
+			}
+			return{
+			"stringToNumb": stringToNumb
+			}
 };
 
-//5. see if Check is within pct Percent of numberRef
+//5. See if Check is within pct Percent of numberRef
 bibiLib.fuzzyMatch = function(numberRef, numberCheck, pct){
 	var bigNum = numberRef + (numberRef * (pct/100));
 	var lilNum = numberRef - (numberRef * (pct/100));
-	var result = ( numberCheck <= bigNum || numberCheck >= lilNum);
-	if(result = true) {
-		return true;
-	} else {
-		return false;
-	}
+	console.log(bigNum, lilNum);
+	if((numberCheck <= bigNum) || (numberCheck >= lilNum)) {
+			console.log("is");
+			} else {
+			console.log("is not");
+			}
+			
+//6.  Title Case 
+
+
+
+
+
+
+
 };
+
+
+
 
 
 
@@ -84,4 +99,6 @@ console.log(bibiLib.checkPhone(123));
 console.log(bibiLib.checkEmail("bladele@me"));
 console.log(bibiLib.checkLink("http://www.apple.com/"));
 console.log(bibiLib.stringToNumb("100"));
+console.log(bibiLib.fuzzyMatch(100, 10, 60));
+console.log(bibiLib.numberSum("I want 10 pens."))
 
